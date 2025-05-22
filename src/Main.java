3c402e835c032141df4f683de20b5b2ab9b0789a
Denis <deniskazarinov@gmail.com>
2025-05-22 16:15:33 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int x = 1100;
-        if (x < 1000) {
-            int x1 = balance + x;
+        int add  = 1100;
+        if (add < 1000) {
+            int x1 = balance + add;
             System.out.println("Баланс телефона:" + x1);
         } else {
-            int i = x + x / 100 + 1 + balance;
+            int i = add / 100 + 1 + (balance + add);
 
             System.out.println("Баланс телефона:" + i);
         }
