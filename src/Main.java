595ef778a169659ed28c99ff5b3f463adb8e3ca8
Denis <deniskazarinov@gmail.com>
2025-05-22 03:39:32 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int balance = 100;
+        int x = 1100;
+        if (x < 1000) {
+            int x1 = balance + x;
+            System.out.println("Баланс телефона:" + x1);
+        } else {
+            int i = x + x / 100 + 1 + balance;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("Баланс телефона:" + i);
         }
     }
 }
+
+
+// Объявляете переменные для входных данных и
+// параметров программы: начального счёта,
+// суммы пополнения и тп
+
+// Условным оператором проверяете, превысила ли
+// сумма пополнения порог, и для этих двух разных
+// сценариев рассчитываете сумму бонуса и выводите
+// на экран.
+
